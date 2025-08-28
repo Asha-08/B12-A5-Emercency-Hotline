09daae6a1252de8cefec0cdd7c2eaf655d1f0c0e
Asha-08 <ashaamina91@gmail.com>
2025-08-28 13:14:13 +0600

    all done

--- script.js
+++ script.js
@@ -28,3 +28,54 @@ for(const button of copyBtns){
         });
     });
 }
+
+// call section
+let countCoin = document.getElementById("count-coin");
+let coins = parseInt(countCoin.textContent);
+
+let callbtns = document.querySelectorAll(".call-btn");
+
+let callHistory = document.getElementById("call-history");
+
+for (let callbtn of callbtns){
+    callbtn.addEventListener("click",function(){
+        if (coins >= 20){
+            let callCard = callbtn.closest(".card");
+            let serviceName = callCard.querySelector(".service-name").textContent;
+            let serviceNumber = callCard.querySelector(".service-number").textContent;
+
+            alert(`calling ${serviceName} : ${serviceNumber}`);
+            coins -= 20;
+            countCoin.textContent = coins;
+
+            // create card
+
+            let newCard = document.createElement("div");
+            newCard.className = "flex justify-between p-3 rounded-xl shadow-xl mb-[4px]";
+            let currentTime = new Date();
+            let time = currentTime.toLocaleTimeString();
+
+            newCard.innerHTML = `
+            <div>
+                    <h1 class="text-xl font-semibold">${serviceName}</h1>
+                    <p>${serviceNumber}</p>
+                </div>
+                <span>${time}</span>
+                `;
+
+                callHistory.appendChild(newCard);
+
+        }
+        else{
+            alert("You do not have enough coin");
+            return;
+        }
+    });
+}
+
+// clear button
+
+let clear = document.getElementById("button-clear");
+clear.addEventListener("click",function(){
+    callHistory.innerHTML = "";
+});
